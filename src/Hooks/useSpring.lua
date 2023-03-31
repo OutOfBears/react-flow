6d170e200431c56196ea26a67757ae56980a9fa2
Jack Fox <0xdeadbeef1@gmail.com>
2023-03-31 16:30:52 -0400

    Implemented animation hooks

---
+++ src/Hooks/useSpring.lua
@@ -0,0 +1,73 @@
+local RunService = game:GetService("RunService")
+
+local React = require(script.Parent.Parent.React)
+local useEffect = React.useEffect
+local useState = React.useState
+local useMemo = React.useMemo
+
+local Utility = script.Parent.Parent.Utility
+local LinearValue = require(Utility.LinearValue)
+local SpringValue = require(Utility.SpringValue)
+local SpringValues = {}
+
+RunService:UnbindFromRenderStep("UPDATE_REACT_SPRING")
+RunService:BindToRenderStep("UPDATE_REACT_SPRING", Enum.RenderPriority.First.Value, function(dt: number)
+	for spring, setValue in pairs(SpringValues) do
+		local didUpdate = spring:Update(dt)
+		local updatedValue = spring:GetValue()
+
+		if not didUpdate then
+			SpringValues[spring] = nil
+			updatedValue = spring:GetGoal()
+		end
+
+		setValue(updatedValue)
+	end
+end)
+
+local function useSpring(initial: LinearValue.LinearValueType, speed: number?, dampening: number?)
+	speed = speed or 20
+	dampening = dampening or 1
+
+	local spring = useMemo(function()
+		return SpringValue.new(initial, speed, dampening)
+	end, {})
+
+	local state, setState = useState(initial)
+
+	useEffect(function()
+		return function()
+			SpringValues[spring] = nil
+		end
+	end, {})
+
+	return state,
+		function(animation: {
+			goal: LinearValue.LinearValueType?,
+			value: LinearValue.LinearValueType?,
+			force: LinearValue.LinearValueType?,
+		})
+			if animation.goal then
+				spring:SetGoal(animation.goal)
+			end
+
+			if animation.value then
+				spring:SetValue(animation.value)
+			end
+
+			if animation.force then
+				spring:Impulse(animation.force)
+			end
+
+			SpringValues[spring] = setState
+		end,
+		function(velocity: LinearValue.LinearValueType)
+			spring:Impulse(velocity)
+			SpringValues[spring] = setState
+		end,
+		function()
+			SpringValues[spring] = nil
+		end
+end
+
+return useSpring
---
+++ src/Hooks/useTween.lua
@@ -0,0 +1,76 @@
+local TweenService = game:GetService("TweenService")
+
+local React = require(script.Parent.Parent.React)
+local useRef = React.useRef
+local useState = React.useState
+local useEffect = React.useEffect
+local useMemo = React.useMemo
+
+local LinearValue = require(script.Parent.Parent.Utility.LinearValue)
+type LinearValue = typeof(LinearValue)
+
+local function useTween(initial: LinearValue.LinearValueType, tweenInfo: TweenInfo)
+	local initialRef = useRef()
+	local goalRef = useRef()
+	local tweenRef = useRef()
+
+	local current, setState = useState(function()
+		return LinearValue.fromValue(initial)
+	end)
+
+	local numberValue = useMemo(function()
+		return Instance.new("NumberValue")
+	end, {})
+
+	useEffect(function()
+		local connection = numberValue:GetPropertyChangedSignal("Value"):Connect(function()
+			setState(initialRef.current:Lerp(goalRef.current, numberValue.Value))
+		end)
+
+		return function()
+			if tweenRef.current then
+				tweenRef.current:Cancel()
+				tweenRef.current = nil
+			end
+
+			connection:Disconnect()
+		end
+	end, {})
+
+	return current:ToValue(),
+		function(animation: {
+			start: LinearValue.LinearValueType?,
+			goal: LinearValue.LinearValueType?,
+		})
+			local startValue = animation.start
+			local endValue = animation.goal
+
+			if not endValue or (goalRef.current and goalRef.current:ToValue() == endValue) then
+				return
+			end
+
+			if not startValue then
+				startValue = current:ToValue()
+			end
+
+			if tweenRef.current then
+				tweenRef.current:Cancel()
+			end
+
+			initialRef.current = LinearValue.fromValue(startValue)
+			goalRef.current = LinearValue.fromValue(endValue)
+
+			numberValue.Value = 0
+
+			tweenRef.current = TweenService:Create(numberValue, tweenInfo, { Value = 1 })
+			tweenRef.current:Play()
+		end,
+		function()
+			if tweenRef.current then
+				tweenRef.current:Cancel()
+				tweenRef.current = nil
+			end
+		end
+end
+
+return useTween
