694d4a9405ec56b6c82c4b8f8a89c2a474ac0efb
Jack Fox <0xdeadbeef1@gmail.com>
2023-04-04 19:26:53 -0400

    Turned into object, and added useStart

--- src/Hooks/useSequence.lua
+++ src/Hooks/useSequence.lua
@@ -4,33 +4,52 @@ local useRef = Roact.useRef
 local Animation = require(script.Parent.Parent.Animation)
 export type Animation = typeof(Animation)
 
-local function useSequence(animations: { [string]: Animation })
-	local sequence = {}
-	local runningRef = useRef()
+local function makeSequence(animations: { [string]: Animation })
+	local currentSequence
 
-	for name, animation in animations do
-		sequence[name] = animation.value
-	end
-
-	return sequence,
-		function()
+	return {
+		play = function(data: { reverse: boolean }?)
+			local reverse = data and data.reverse
 			local nonce = tick()
-			runningRef.current = nonce
+			currentSequence = nonce
 
 			for _, animation in animations do
+				animation.useStart.current = true
+
 				task.delay(animation.timeStep or 0, function()
-					if runningRef.current == nonce then
-						animation.play()
+					if currentSequence == nonce then
+						animation.useStart.current = false
+						animation[reverse and "playReverse" or "play"]()
 					end
 				end)
 			end
 		end,
-		function()
-			runningRef.current = nil
+
+		stop = function()
+			currentSequence = nil
 			for _, animation in animations do
 				animation.stop()
 			end
-		end
+		end,
+	}
+end
+
+local function useSequence(animations: { [string]: Animation })
+	local sequence = {}
+	local sequenceRef = useRef()
+	local currentSequence = sequenceRef.current
+
+	if not currentSequence then
+		currentSequence = makeSequence(animations)
+		sequenceRef.current = currentSequence
+	end
+
+	for name, animation in animations do
+		animation.useStart = useRef(true)
+		sequence[name] = animation.useStart.current and animation.start or animation.value
+	end
+
+	return sequence, currentSequence.play, currentSequence.stop
 end
 
 return useSequence
